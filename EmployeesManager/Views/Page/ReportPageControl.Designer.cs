3e57169affa0fddd22e994840372b0f7214326fb
MeinLiX <meinlix@ukr.net>
2022-07-08 17:17:50 +0300

    Add report page

--- EmployeesManager/Views/Page/ReportPageControl.Designer.cs
+++ EmployeesManager/Views/Page/ReportPageControl.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.materialCard = new MaterialSkin.Controls.MaterialCard();
             this.materialLabelHeader = new MaterialSkin.Controls.MaterialLabel();
+            this.materialButtonExport = new MaterialSkin.Controls.MaterialButton();
+            this.materialComboBoxExportType = new MaterialSkin.Controls.MaterialComboBox();
+            this.saveFileDialogExport = new System.Windows.Forms.SaveFileDialog();
             this.materialCard.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,16 +64,70 @@
             this.materialLabelHeader.TabIndex = 6;
             this.materialLabelHeader.Text = "Report";
             // 
+            // materialButtonExport
+            // 
+            this.materialButtonExport.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButtonExport.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButtonExport.Depth = 0;
+            this.materialButtonExport.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.materialButtonExport.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.materialButtonExport.HighEmphasis = true;
+            this.materialButtonExport.Icon = null;
+            this.materialButtonExport.Location = new System.Drawing.Point(0, 348);
+            this.materialButtonExport.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButtonExport.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButtonExport.Name = "materialButtonExport";
+            this.materialButtonExport.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButtonExport.Size = new System.Drawing.Size(790, 36);
+            this.materialButtonExport.TabIndex = 3;
+            this.materialButtonExport.Text = "Export";
+            this.materialButtonExport.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Outlined;
+            this.materialButtonExport.UseAccentColor = false;
+            this.materialButtonExport.UseVisualStyleBackColor = true;
+            this.materialButtonExport.Click += new System.EventHandler(this.materialButtonExport_Click);
+            // 
+            // materialComboBoxExportType
+            // 
+            this.materialComboBoxExportType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialComboBoxExportType.AutoResize = true;
+            this.materialComboBoxExportType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialComboBoxExportType.Depth = 0;
+            this.materialComboBoxExportType.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.materialComboBoxExportType.DropDownHeight = 174;
+            this.materialComboBoxExportType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.materialComboBoxExportType.DropDownWidth = 121;
+            this.materialComboBoxExportType.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialComboBoxExportType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialComboBoxExportType.FormattingEnabled = true;
+            this.materialComboBoxExportType.Hint = "Select export type";
+            this.materialComboBoxExportType.IntegralHeight = false;
+            this.materialComboBoxExportType.ItemHeight = 43;
+            this.materialComboBoxExportType.Items.AddRange(new object[] {
+            "*.txt"});
+            this.materialComboBoxExportType.Location = new System.Drawing.Point(328, 67);
+            this.materialComboBoxExportType.MaxDropDownItems = 4;
+            this.materialComboBoxExportType.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialComboBoxExportType.Name = "materialComboBoxExportType";
+            this.materialComboBoxExportType.Size = new System.Drawing.Size(121, 49);
+            this.materialComboBoxExportType.StartIndex = 0;
+            this.materialComboBoxExportType.TabIndex = 4;
+            this.materialComboBoxExportType.UseAccent = false;
+            // 
             // ReportPageControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.materialComboBoxExportType);
+            this.Controls.Add(this.materialButtonExport);
             this.Controls.Add(this.materialCard);
             this.Name = "ReportPageControl";
             this.Size = new System.Drawing.Size(790, 384);
+            this.ClientSizeChanged += new System.EventHandler(this.ReportPageControl_ClientSizeChanged);
             this.materialCard.ResumeLayout(false);
             this.materialCard.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,5 +135,8 @@
 
         private MaterialSkin.Controls.MaterialCard materialCard;
         private MaterialSkin.Controls.MaterialLabel materialLabelHeader;
+        private MaterialSkin.Controls.MaterialButton materialButtonExport;
+        private MaterialSkin.Controls.MaterialComboBox materialComboBoxExportType;
+        private SaveFileDialog saveFileDialogExport;
     }
 }
--- EmployeesManager/Views/Page/ReportPageControl.cs
+++ EmployeesManager/Views/Page/ReportPageControl.cs
@@ -1,5 +1,7 @@
 ﻿using EmployeesManager.Context;
 using EmployeesManager.Views.Page.Interfaces;
+using MaterialSkin.Controls;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -34,5 +36,54 @@ public partial class ReportPageControl : UserControl, IControllWithServices
         CheckServices();
 
         return Task.CompletedTask;
+    }
+
+    private void ReportPageControl_ClientSizeChanged(object sender, EventArgs e)
+    {
+        materialComboBoxExportType.Left = (this.ClientSize.Width - materialComboBoxExportType.Width) / 2;
+    }
+
+    private async void materialButtonExport_Click(object sender, EventArgs e)
+    {
+        await (materialComboBoxExportType.SelectedItem switch
+        {
+            string ext when ext == "*.txt" => exportTXT()
+        });
+    }
+
+    private async Task exportTXT()
+    {
+        using SaveFileDialog saveFileDialogExport = new();
+        saveFileDialogExport.Title = "Choose file to export";
+        saveFileDialogExport.CheckPathExists = true;
+        saveFileDialogExport.Filter = "Text Files | *.txt";
+        saveFileDialogExport.DefaultExt = "txt";
+        saveFileDialogExport.RestoreDirectory = true;
+        if (saveFileDialogExport.ShowDialog() == DialogResult.OK)
+        {
+            using (StreamWriter writer = new(Path.GetFullPath(saveFileDialogExport.FileName), false))
+            {
+                await _mainCTX.Employees.Include(e => e.Department)
+                                        .Include(e => e.Position)
+                                        .Include(e => e.Salary)
+                                        .ForEachAsync(e =>
+                {
+                    writer.WriteLine($"{e.GetFullName()}\\{e.Department.Department}\\{e.Position.PositionName}\\{e.Salary.Salary}\\{e.Salary.KPI}\\{e.Salary.GetPremium()}");
+                });
+            };
+            new MaterialSnackBar($"Export successful complete.", 1000).Show(this);
+        }
+        else
+        {
+            new MaterialSnackBar($"Export canceled.", 1000).Show(this);
+        }
+    }
+    private MainForm? GetParentWindow()
+    {
+        if (this.Parent is TabPage parentPage)
+            if (parentPage.Parent is MaterialTabControl parentTabControll)
+                if (parentTabControll.Parent is MainForm parentForm)
+                    return parentForm;
+        return null;
     }
 }
